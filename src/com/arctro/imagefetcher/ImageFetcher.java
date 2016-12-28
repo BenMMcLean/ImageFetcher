51073a3a8203abfe09ad330f0f21522f14486572
Ben McLean <benmclean007@gmail.com>
2016-12-28 20:18:18 +1100

    Moved the IFS class inside ImageFetcher

--- src/com/arctro/imagefetcher/ImageFetcher.java
+++ src/com/arctro/imagefetcher/ImageFetcher.java
@@ -83,4 +83,31 @@ public class ImageFetcher {
 		
 		return url;
 	}
+	
+	public static class IFS {
+		String name;
+		String value;
+		
+		public IFS(String name, String value) {
+			super();
+			this.name = name;
+			this.value = value;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
 }
