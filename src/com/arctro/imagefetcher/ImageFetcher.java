ce1a30e39b34053b2d550a51c0d2b490b6a9ae15
Ben McLean <benmclean007@gmail.com>
2016-12-29 16:12:08 +1100

    Fixed Giphy support

--- src/com/arctro/imagefetcher/ImageFetcher.java
+++ src/com/arctro/imagefetcher/ImageFetcher.java
@@ -20,10 +20,9 @@ import org.jsoup.select.Elements;
 
 public class ImageFetcher {
 	
-	public IFS[] search = {new IFS("[name=twitter:image]","content"),  new IFS("[name=og:url]","url"), new IFS("[property=og:image]","content"), new IFS("[name=og:image]","href")};
+	public IFS[] search = {new IFS("[name=twitter:image]","content"),  new IFS("[property=og:url]","url"), new IFS("[property=og:image]","content"), new IFS("[name=og:image]","href")};
 	
 	public Detector detector = TikaConfig.getDefaultConfig().getDetector();
-	
 	
 	public ImageFetcher(){
 		
