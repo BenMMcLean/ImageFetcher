e48db5af97439dab133b48fbf5675da9b2029d45
Ben McLean <benmclean007@gmail.com>
2016-12-29 18:02:07 +1100

    Added support for user defined HostFetchers

--- src/com/arctro/imagefetcher/hosts/HostFetcherFactory.java
+++ src/com/arctro/imagefetcher/hosts/HostFetcherFactory.java
@@ -1,10 +1,16 @@
 package com.arctro.imagefetcher.hosts;
 
+import java.util.HashMap;
+
 /**
  * Gets the appropriate HostFetcher for the given host
  * @author Ben McLean
  */
 public class HostFetcherFactory {
+	
+	//Stores user defined HostFetchers
+	public static HashMap<String, HostFetcher> customHosts = new HashMap<String, HostFetcher>();
+	
 	/**
 	 * Returns the appropriate HostFetcher for the given host
 	 * @param host The host to get the HostFetcher for
@@ -21,6 +27,20 @@ public class HostFetcherFactory {
 		case "twitter.com":
 			return new TwitterHostFetcher();
 		}
+		
+		if(customHosts.containsKey(host)){
+			return customHosts.get(host);
+		}
+		
 		return null;
+	}
+	
+	/**
+	 * Add a user defined HostFetcher
+	 * @param host The host to associate the HostFetcher with
+	 * @param hostFetcher The HostFetcher
+	 */
+	public static void addCustomHost(String host, HostFetcher hostFetcher){
+		customHosts.put(host, hostFetcher);
 	}
 }
